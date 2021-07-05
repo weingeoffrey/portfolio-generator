ed3c450f8f072438081090bc551657a43a992710
weinge <wein.geoffrey@gmail.com>
2021-07-04 19:06:26 -0500

    created function to generate html page based off of inputted user's name and github

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [username, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(username, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio commplete! Check out index.html to see the output!');
+});
